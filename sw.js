9b582adaaa158f907f632e4b3cffeed4af2b912a
fabiveL00 <fabian.schweikert04@gmail.com>
2026-09-21 19:53:41 +0200

    Add files via upload
    abo kategorie

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Haushaltsbuch – Service Worker: speichert nur die App-Hülle, nie deine Daten.
-const CACHE = "haushaltsbuch-v6";
+const CACHE = "haushaltsbuch-v7";
 const SHELL = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
