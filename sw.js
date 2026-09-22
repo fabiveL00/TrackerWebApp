35546bf557d285b544a6f77b8cda335fea4646ec
fabiveL00 <fabian.schweikert04@gmail.com>
2026-09-23 00:36:49 +0200

    Add files via upload
    kreditkarten abrechnung

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Haushaltsbuch – Service Worker: speichert nur die App-Hülle, nie deine Daten.
-const CACHE = "haushaltsbuch-v14";
+const CACHE = "haushaltsbuch-v15";
 const SHELL = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
